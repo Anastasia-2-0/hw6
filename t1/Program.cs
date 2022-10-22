1f0ab16586362a8af4d44c5b01ecd0b002633b11
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-22 16:36:52 +0300

    решение задач

---
+++ t1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("введите количество чисел:");
+int size = int.Parse(Console.ReadLine());
+int [] array = new int [size];
+
+VvodChisel(array);
+PrintArray(array);
+Console.WriteLine($"Чисел больше 0 - {PositiveNumbers(array)}");
+
+void VvodChisel(int[] arra)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите число: ");
+        arra[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(",");
+        position++;
+    }
+   
+}
+
+int PositiveNumbers(int[] arra)
+{
+int positiveSum = 0;
+   for (int i = 0; i < arra.Length; i++)
+   {
+    if(arra[i] > 0)
+        positiveSum += 1;
+   }
+   return positiveSum;
+}
+
---
+++ t2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите  b1 = ");
+float b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите  k1 = ");
+float k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите  b2 = ");
+float b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите  k2 = ");
+float k2 = int.Parse(Console.ReadLine());
+
+float Spotx = 0;
+float Spoty = 0;
+
+Spotx = ((b2 - b1) / (k1 - k2));
+Spoty = (k1 * ((b2 - b1) / (k1 - k2)) + b1);
+
+Console.WriteLine($"Точка персечения прямых ({Spotx};{Spoty})");
